82b64f7771abb96758d69686f19aac8f3ce365df
Matt Buchovecky <mbuchove@gmail.com>
2015-10-06 10:49:34 -0700

    added options for combo_lt.sh

--- shellScripts/combo_lt.sh
+++ shellScripts/combo_lt.sh
@@ -16,13 +16,25 @@ else
     buildOptions="-TelID=0,1,2,3 -Azimuth=0,45,90,135,180,225,270,315 -Zenith=00,20,30,35,40,45,50,55,60,65 -AbsoluteOffset=0.00,0.50,0.75,0.25,1.00 -Noise=4.24,5.21,6,6.68,7.27,7.82,8.33,9.20,10.19,11.17,12.17"
 fi
 
-runMode=qsub
-#runMode=bash
+runMode=cat
 
 finalTable=lt_Oct2012_${array}_ATM${atm}_7samples_vegas254_allOffsets_LZA_d1p38.root
 
 logFile=$GC/log/tables/combine_lt_${array}_ATM${atm}_allOffsets_LZA.txt
 test -f $logFile && mv $logFile $GC/backup/logTable/
+
+while getopts qr: FLAG; do 
+    case $FLAG in
+	q)
+	    runMode=qsub ;; 
+	r) 
+	    runMode=$OPTARG ;; 
+	?)
+	    echo "Option -${BOLD}${OPTARG}${NORM} not allowed"
+    esac # option cases
+done # while loop over options 
+shift $((OPTIND-1))
+
 
 $runMode <<EOF
 
